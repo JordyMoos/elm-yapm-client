7077f6a51adf8132c0c334aaa59c8826eed9f833
Jordy Moos <jordymoos@gmail.com>
2017-05-21 18:48:16 +0200

    Continue the refactoring

--- src/Ports.elm
+++ src/Ports.elm
@@ -1,5 +1,7 @@
 port module Ports exposing (..)
 
+import Json.Encode exposing (Value)
+
 
 type alias LibraryData =
     { library : String
@@ -11,25 +13,61 @@ type alias MasterKey =
     String
 
 
+type alias ElementId =
+    String
+
+
+type alias Library =
+    { blob : String
+    , libraryVersion : Int
+    , apiVersion : Int
+    , modified : Int
+    }
+
+
+type alias EncryptLibraryDataContent =
+    { oldMasterKey : Maybe MasterKey
+    , oldLibraryData : Maybe LibraryData
+    , newMasterKey : Maybe MasterKey
+    , passwords : List Password
+    }
+
+
+type alias UploadLibraryContent =
+    { oldHash : String
+    , newHash : String
+    , libraryData : LibraryData
+    }
+
+
 type alias ParseLibraryDataContent =
     { masterKey : Maybe MasterKey
     , libraryData : Maybe LibraryData
     }
 
 
+type alias Password =
+    { comment : String
+    , password : String
+    , title : String
+    , url : String
+    , username : String
+    }
+
+
 port parseLibraryData : ParseLibraryDataContent -> Cmd msg
-
-
-port error : (String -> msg) -> Sub msg
-
-
-port passwords : (List String -> msg) -> Sub msg
 
 
 port encryptLibraryData : EncryptLibraryDataContent -> Cmd msg
 
 
-port uploadLibrary : (UploadLibraryContent -> msg) -> Sub msg
-
-
 port copyPasswordToClipboard : ElementId -> Cmd msg
+
+
+port error : (Value -> msg) -> Sub msg
+
+
+port loginSuccess : (Value -> msg) -> Sub msg
+
+
+port uploadLibrary : (Value -> msg) -> Sub msg
