18bbed4c15e06712d159a9615461dce7ca4060e4
Jordy Moos <jordymoos@gmail.com>
2017-05-21 21:13:55 +0200

    Refactor models to data and communicate with ports only via json WIP)

---
+++ src/Data/Password.elm
@@ -0,0 +1,50 @@
+module Data.Password exposing (Password, decoder, encode, decodePasswordFromJson)
+
+import Json.Decode as Decode exposing (Decoder)
+import Json.Decode.Pipeline as Pipeline exposing (decode, required)
+import Json.Encode as Encode exposing (Value)
+import Util exposing ((=>))
+
+
+type alias Password =
+    { comment : String
+    , password : String
+    , title : String
+    , url : String
+    , username : String
+    }
+
+
+passwordDecoder : Decoder Password
+passwordDecoder =
+    decode Password
+        |> required "comment" Decode.string
+        |> required "password" Decode.string
+        |> required "title" Decode.string
+        |> required "url" Decode.string
+        |> required "username" Decode.string
+
+
+passwordsDecoder : Decoder (List Password)
+passwordsDecoder =
+    decode (List Password)
+        |> required "passwords" (Decode.list passwordDecoder)
+
+
+encode : Password -> Value
+encode password =
+    Encode.object
+        [ "comment" => Encode.string password.comment
+        , "password" => Encode.string password.password
+        , "title" => Encode.string password.title
+        , "url" => Encode.string password.url
+        , "username" => Encode.string password.username
+        ]
+
+
+decodePasswordsFromJson : Value -> Maybe Password
+decodePasswordsFromJson json =
+    json
+        |> Decode.decodeValue Decode.string
+        |> Result.toMaybe
+        |> Maybe.andThen (Decode.decodeString decoder >> Result.toMaybe)
